bc5aea4d7b430a5a6ab0f8ecb17f2fef5508eef1
al-ghalib <abdullah.ice.ru@gmail.com>
2024-11-19 17:50:55 +0600

    feat(api): add user registration API view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import RegisterSerializer
 
-# Create your views here.
+
+@api_view(['POST'])
+def register(request):
+    if request.method == 'POST':
+        # RegisterSerializer to validate the input data and create the user
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response({
+                'detail': 'User registered successfully.'
+            }, status=status.HTTP_201_CREATED)
+
+        # Return errors while validation fails
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
