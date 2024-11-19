285b62b6f4808e18732f8b773d8de0ddfa146743
al-ghalib <abdullah.ice.ru@gmail.com>
2024-11-19 19:00:50 +0600

    feat(models): add practice history model.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from quiz.models import Question, Choice
 
-# Create your models here.
+class PracticeHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    chosen_answer = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    is_correct = models.BooleanField()
+    attempted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.question.text} - {'Correct' if self.is_correct else 'Incorrect'}"
